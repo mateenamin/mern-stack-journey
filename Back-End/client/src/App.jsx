1f27edab5fab70305f171c8daba04c395a7bb6d7
Mateen Amin <mateenamin1122@gmail.com>
2026-06-15 05:04:37 +0500

    Front-end Systemup Done

---
+++ Back-End/client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+const App = () => {
+  return (
+    <div><Login/>
+    <Register/>
+    </div>
+    
+  )
+}
+
+export default App
